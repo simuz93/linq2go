24d6c3b69f327bebe2d723c00dc1432c4b193228
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-08-28 12:23:26 +0200

    Fixed FirstOrNil

--- first.go
+++ first.go
@@ -2,9 +2,9 @@ package linq2go
 
 // FirstOrNil returns the first occurrence of an element that matches the given function or nil if no element matches it
 func (s *Slice[T]) FirstOrNil(fn func(T) bool) *T {
-	for _, v := range s.s {
-		if fn(v) {
-			return &v
+	for i := range s.s {
+		if fn(s.s[i]) {
+			return &s.s[i]
 		}
 	}
 
