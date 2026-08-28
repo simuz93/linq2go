bdd829070feaf8c35fa4c83843bb09bba0a90689
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-08-28 12:24:00 +0200

    Added guards to Skip

--- skip.go
+++ skip.go
@@ -2,5 +2,7 @@ package linq2go
 
 // TrimEnd trims the the given value from the end of the slice
 func (s *Slice[T]) Skip(n int) *Slice[T] {
+	n = max(n, 0)
+	n = min(n, len(s.s))
 	return FromSlice(s.s[n:])
 }
