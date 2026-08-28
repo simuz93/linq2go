7114d3a3ec18d5924f04b1a06516336237d1d078
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-08-28 12:24:05 +0200

    Added guards to Take

--- take.go
+++ take.go
@@ -2,5 +2,7 @@ package linq2go
 
 // TrimEnd trims the the given value from the end of the slice
 func (s *Slice[T]) Take(n int) *Slice[T] {
-	return FromSlice(s.s[:n])
+	n = max(n, 0)
+	n = min(n, len(s.s))
+	return FromSlice(s.s[:n:n])
 }
