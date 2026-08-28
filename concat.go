45fd4363d3e161a1c13e1501367c484fdf37e8b9
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-08-28 12:23:05 +0200

    Fixed concat

--- concat.go
+++ concat.go
@@ -3,6 +3,6 @@ package linq2go
 import "slices"
 
 // Concat concatenates the given slices. Shortcut for slices.Concat, left here for retrocompatibility
-func (s *Slice[T]) Concat(values ...[]T) *Slice[T] {
-	return FromSlice(slices.Concat(values...))
+func (s *Slice[T]) Concat(values []T) *Slice[T] {
+	return FromSlice(slices.Concat(s.s, values))
 }
