bbe701c92b508644838b15529162e68ef7932f71
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-08-28 12:24:26 +0200

    To methods now clone elements

--- to.go
+++ to.go
@@ -1,14 +1,19 @@
 package linq2go
 
+import (
+	"maps"
+	"slices"
+)
+
 func (s *Slice[T]) ToSlice() []T {
-	return s.s
+	return slices.Clone(s.s)
 }
 
 // MapFromSlice creates a map from a slice by applying the given function to each element of the slice
-func (s *Slice[T]) ToMap[K comparable, V any](fn func(elem *T, index int) (K, V)) map[K]V {
+func (s *Slice[T]) ToMap[K comparable, V any](fn func(elem T, index int) (K, V)) map[K]V {
 	m := map[K]V{}
 	for i := range s.s {
-		key, value := fn(&s.s[i], i)
+		key, value := fn(s.s[i], i)
 		if _, ok := m[key]; !ok {
 			m[key] = value
 		}
@@ -17,7 +22,7 @@ func (s *Slice[T]) ToMap[K comparable, V any](fn func(elem *T, index int) (K, V)
 }
 
 func (m *Map[K, V]) ToMap() map[K]V {
-	return m.m
+	return maps.Clone(m.m)
 }
 
 // MapToSlice converts a map to a slice by applying the given function to each element of the map. The resulting slice will be unordered
@@ -28,4 +33,14 @@ func (m *Map[K, V]) ToSlice[T any](fn func(key K, value V) T) []T {
 		s = append(s, fn(key, m.m[key]))
 	}
 	return s
+}
+
+func (g *Group[K, V]) ToMap() map[K][]V {
+	result := map[K][]V{}
+
+	for k, v := range g.m {
+		result[k] = slices.Clone(v)
+	}
+
+	return result
 }
