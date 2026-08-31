f4f71afbb64aa5b19af245c183a2ae2c850adc9c
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-08-31 09:22:03 +0200

    Memory management improvement in Distinct

--- distinct.go
+++ distinct.go
@@ -2,13 +2,13 @@ package linq2go
 
 // Distinct returns a new slice without duplicates, keeping the first element for each key selected by fn
 func (s *slice[T]) Distinct[K comparable](fn func(T) K) *slice[T] {
-	distinct := make(map[K]any)
+	distinct := make(map[K]struct{})
 	result := []T{}
 
 	for _, v := range s.s {
 		item := fn(v)
 		if _, ok := distinct[item]; !ok {
-			distinct[item] = nil
+			distinct[item] = struct{}{}
 			result = append(result, v)
 		}
 	}
