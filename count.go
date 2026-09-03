f113237c9217abd1e98925016dd323ae89f74c4c
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-09-03 10:09:18 +0200

    Added count methods

---
+++ count.go
@@ -0,0 +1,22 @@
+package linq2go
+
+// Count returns the length of the slice
+//
+//	FromSlice([]int{1, 2, 3}).Count() // 3
+func (s *slice[T]) Count() int {
+	return len(s.values)
+}
+
+// Count returns the number of key-value pairs in the map
+//
+//	FromMap(map[string]int{"a": 1, "b": 2}).Count()) // 2
+func (s *dictionary[K, V]) Count() int {
+	return len(s.values)
+}
+
+// Count returns the number of key-value pairs in the group
+//
+//	FromMap(map[string]int{"a": 2, "b": 4}).Group(func(k string, v int) bool { return v%2 == 0 }).Count()) // 1
+func (s *group[K, V]) Count() int {
+	return len(s.values)
+}
