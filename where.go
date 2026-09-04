32dc8a72f524e3c4e51b845a3479dd2eb1af0b66
Simone Serra <35728320+simuz93@users.noreply.github.com>
2026-09-04 08:22:46 +0200

    Added where functions

--- where.go
+++ where.go
@@ -14,3 +14,33 @@ func (s *slice[T]) Where(fn func(T) bool) *slice[T] {
 
 	return newSlice(result)
 }
+
+// Where returns a new dictionary with the elements satisfying fn
+//
+//	FromMap(map[string]int{"a": 1, "b": 2}).Where(func(k string, v int) bool { return v%2 == 0 }).ToMap() // map[b:2]
+func (m *dictionary[K, V]) Where(fn func(K, V) bool) *dictionary[K, V] {
+	result := map[K]V{}
+
+	for k, v := range m.values {
+		if fn(k, v) {
+			result[k] = v
+		}
+	}
+
+	return newDictionary(result)
+}
+
+// Where returns a new group with the buckets satisfying fn
+//
+//	FromSlice([]int{1, 2, 3}).Group(func(v int) bool { return v > 1 }).Where(func(k bool, bucket []int) bool { return len(bucket) > 1 }).ToMap() // map[true:[2 3]]
+func (g *group[K, V]) Where(fn func(K, []V) bool) *group[K, V] {
+	result := map[K][]V{}
+
+	for k, v := range g.values {
+		if fn(k, v) {
+			result[k] = v
+		}
+	}
+
+	return newGroup(result)
+}
